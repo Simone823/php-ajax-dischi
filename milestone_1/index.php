cb67e8c47f403b902d984ef033b88405aa2d50ad
Simone823 <simone.daglio823@outlook.it>
2022-03-31 15:58:45 +0200

    main albums foreach db.php

---
+++ milestone_1/index.php
@@ -0,0 +1,57 @@
+<?php
+    include '../milestone_1/db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Milestone 1</title>
+</head>
+<body>
+    
+
+    <!-- Main -->
+    <main>
+
+        <!-- Wrapper album -->
+        <div class="wrapper_albums">
+
+            <!-- Ciclo foreach array albums -->
+            <?php
+                foreach ($albums as $element) {
+                    // var_dump($element);
+                    ?>
+
+                    <!-- Album -->
+                    <div class="album">
+
+                        <!-- Wrapper Immagine -->
+                        <div class="img_wrapper">
+                            <img src="<?php echo $element['poster'] ?>" alt="">
+                        </div>
+
+                        <!-- Wrapper description -->
+                        <div class="description_wrapper">
+                            <h1 class="title"> <?php echo $element['title'] ?> </h1>
+                            <h3 class="author"> <?php echo $element['author'] ?> </h3>
+                            <h3 class="year"> <?php echo $element['year'] ?> </h3>
+                        </div>
+
+                    </div>
+
+                    <?php  
+                }
+            ?>
+        </div>
+    </main>
+
+</body>
+</html>
